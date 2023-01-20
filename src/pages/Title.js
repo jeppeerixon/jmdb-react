de319185e7792a3cc1adb2ed97ddc10a6de649b4
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-01-20 18:25:50 +0100

    :fire: Beginning of title page for movie info

--- src/pages/Title.js
+++ src/pages/Title.js
@@ -1,5 +1,16 @@
+import { useLocation } from "react-router-dom";
+
 const Title = () => {
-    return <h1>Title</h1>;
-  };
-  
+  const location = useLocation();
+
+  // get userId
+  let movieID = location.state.movieId;
+
+  return (
+  <>
+  <h1>Title</h1>
+  <p> { movieID }</p>
+  </>
+  )}; 
+   
   export default Title;
